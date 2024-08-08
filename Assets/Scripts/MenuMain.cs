acd0518ab242f8b1a7f336e740f2578e93e5a39e
Ilya <prosoulk2017@gmail.com>
2024-08-08 18:22:38 +0200

    added main menu

---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuMain : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
